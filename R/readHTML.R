ae267fb8b181bccb9205eb4b0357b25f6e986949
Aaron Olsen <aaronolsen@users.noreply.github.com>
2016-12-13 00:41:22 -0500

    Renamed function

--- R/readHTML.R
+++ R/readHTML.R
@@ -1,4 +1,4 @@
-readSVG <- function(file){
+readHTML <- function(file){
 	## Reads an svg/html file and returns svg elements as list
 
 	# Set distinctive line break string (not likely to be used in text fields)
@@ -7,6 +7,14 @@ readSVG <- function(file){
 	# Read in lines and collapse into string with distinct line breaks
 	read_lines <- paste(readLines(file), collapse=br_str)
 	
+	# Get javascript parameters
+	jsp_start <- regexpr(pattern=paste0('<script type="text/javascript" >', br_str, '[\t]*// Parameters set by user'), text=read_lines)
+	jsp_stop <- regexpr(pattern=paste0(br_str, '[\t]*</script>', br_str), text=read_lines)
+	
+	# Extract string between start and stop
+	jsp_vec <- strsplit(substr(x=read_lines, start=jsp_start, stop=jsp_stop+attr(jsp_stop,"match.length")), br_str)[[1]]
+	jsp_vec <- gsub('^[\t]+var |;$', '', jsp_vec[3:(length(jsp_vec)-2)])
+
 	# Get start and stop points of svg structure
 	svg_start <- regexpr(pattern=paste0('<svg_doc xmlns="http://www.w3.org/2000/svg" version="[0-9.]+" style="visibility:hidden;" >', br_str), text=read_lines)
 	svg_stop <- regexpr(pattern=paste0(br_str, '[\t]*</svg_doc>', br_str), text=read_lines)
@@ -14,22 +22,55 @@ readSVG <- function(file){
 	# Extract string between start and stop
 	svg_vec <- strsplit(substr(x=read_lines, start=svg_start, stop=svg_stop+attr(svg_stop,"match.length")), br_str)[[1]]
 	svg_vec <- svg_vec[2:(length(svg_vec)-2)]
-	
+
+	# Properties to split at comma
+	split_comma <- c('r', 'stroke', 'fill', 'opacity', 'stroke-width', 'fill-opacity', 'stroke-opacity')
+
+	# Vector of point indices for pathC, if present
+	point_idx <- c()
+	point_num <- 1
+
 	# Create lists and vectors
-	svg.list <- as.list(rep(NA, length(svg_vec)))
+	params <- list()
+	shapes <- as.list(rep(NA, length(svg_vec)))
 	z.index <- rep(NA, length(svg_vec))
+
+	# Read each item into list
+	for(lnum in 1:length(jsp_vec)){
+		
+		# Split at =
+		str_split <- gsub(' ', '', strsplit(jsp_vec[lnum], '=')[[1]])
+		
+		if(str_split[2] == "'NA'"){
+			params[[str_split[1]]] <- NA
+			next
+		}
+		if(!grepl("'", str_split[2])){
+			params[[str_split[1]]] <- as.numeric(str_split[2])
+			next
+		}
+		params[[str_split[1]]] <- gsub("'", "", str_split[2])
+	}
+	
+	# Add other parameters
+	param_names <- c('zoom', 'maxzoom', 'eyez')
+	for(param_name in param_names){
+		str_search <- regexpr(pattern=paste0('[\t]*var ', param_name, ' = [-0-9]+;', br_str), text=read_lines)
+		params[[param_name]] <- as.numeric(strsplit(substr(x=read_lines, start=str_search, stop=str_search+attr(str_search,"match.length")), '=|;')[[1]][2])
+	}
+	params[['depth']] <- floor(params[['zoom']] * (params[['eyez']] - params[['maxzoom']]) / 100 + params[['eyez']])
 
 	# Read each item into list
 	for(lnum in 1:length(svg_vec)){
 
 		# Define as list
-		svg.list[[lnum]] <- list()
+		shapes[[lnum]] <- list()
 
 		# Get string in line
 		svg_line <- svg_vec[lnum]
 
 		# Identify tag
-		svg.list[[lnum]]$type <- type <- substr(svg_line, 3, regexpr(pattern=' ', text=svg_line)-1)
+		shapes[[lnum]]$type <- type <- substr(svg_line, 3, regexpr(pattern=' ', text=svg_line)-1)
 
 		# If text, read text between tags and remove
 		if(type == 'text'){
@@ -38,7 +79,7 @@ readSVG <- function(file){
 			text_start <- regexpr(pattern=' >', text=svg_line)+2
 
 			# Get text string
-			svg.list[[lnum]]$value <- gsub('</text>$', '', substr(x=svg_line, start=text_start, stop=nchar(svg_line)))
+			shapes[[lnum]]$value <- gsub('</text>$', '', substr(x=svg_line, start=text_start, stop=nchar(svg_line)))
 			
 			# Remove end tag
 			svg_line <- substr(x=svg_line, start=1, stop=text_start-3)
@@ -65,67 +106,103 @@ readSVG <- function(file){
 			value <- paste(str_split[2:length(str_split)], collapse='="')
 			
 			# Remove final "
-			svg.list[[lnum]][[str_split[1]]] <- substr(value, 1, nchar(value)-1)
+			shapes[[lnum]][[str_split[1]]] <- substr(value, 1, nchar(value)-1)
 			
 			# Check if numeric
-			if(!is.na(suppressWarnings(as.numeric(svg.list[[lnum]][[str_split[1]]])))) svg.list[[lnum]][[str_split[1]]] <- as.numeric(svg.list[[lnum]][[str_split[1]]])
+			if(!is.na(suppressWarnings(as.numeric(shapes[[lnum]][[str_split[1]]])))) shapes[[lnum]][[str_split[1]]] <- as.numeric(shapes[[lnum]][[str_split[1]]])
 		}
 		
-		# Parse path into matrix
-		if(type == 'path'){
+		if(type == 'point'){
+			point_idx[point_num] <- lnum
+			point_num <- point_num + 1
+		}
+		
+		if(type == 'pathC') shapes[[lnum]][['d']] <- as.numeric(strsplit(shapes[[lnum]][['d']], ',')[[1]])
 
+		## Copy coordinate elements into matrices
+		if(type == 'path'){
 			# Split path string by separating letters and numbers
-			d_split <- strsplit_alphanumeric(xmlAttrs(r[[i]])[['d']])
+			d_split <- splitAlphaNum(shapes[[lnum]][['d']])
 			xyz <- matrix(NA, nrow=length(d_split$numeric)/3, ncol=3)
 			for(k in 1:(length(d_split$numeric)/3)) xyz[k ,] <- d_split$numeric[(k*3-2):(k*3)]
-			shapes[[xmlName(r[[i]])]][[j]][['point_type']] <- d_split$alpha
+			shapes[[lnum]][['dtype']] <- d_split$alpha
+			shapes[[lnum]][['xyz']] <- xyz
+		}
 
-			letter_split <- strsplit(svg.list[[lnum]][['d']], '[A-Za-z]')[[1]]
-			dmat <- matrix(NA, nrow=0, ncol=3)
-			for(i in 1:length(letter_split)){
-				pt_split <- suppressWarnings(as.numeric(strsplit(letter_split[i], ' ')[[1]]))
-				if(length(pt_split) == 0) next
-				dmat <- rbind(dmat, pt_split[!is.na(pt_split)])
+		if(type %in% c('point', 'text')){
+			if(grepl(',', shapes[[lnum]][['x']])){
+				shapes[[lnum]][['xyz']] <- cbind(
+					as.numeric(strsplit(shapes[[lnum]][['x']], ',')[[1]]), 
+					as.numeric(strsplit(shapes[[lnum]][['y']], ',')[[1]]), 
+					as.numeric(strsplit(shapes[[lnum]][['z']], ',')[[1]])
+				)
+			}else{
+				shapes[[lnum]][['xyz']] <- matrix(unlist(shapes[[lnum]][c('x', 'y', 'z')]), nrow=1, ncol=3)
 			}
-			svg.list[[lnum]][['dmat']] <- dmat
 		}
 
-		if(type == c('point', 'text') && grepl(',', svg.list[[lnum]][['x']])){
-			svg.list[[lnum]][['xyz']] <- cbind(
-				as.numeric(strsplit(svg.list[[lnum]][['x']], ',')[[1]]), 
-				as.numeric(strsplit(svg.list[[lnum]][['y']], ',')[[1]]), 
-				as.numeric(strsplit(svg.list[[lnum]][['z']], ',')[[1]])
-			)
+		if(type == 'circle'){
+			if(grepl(',', shapes[[lnum]][['cx']])){
+				shapes[[lnum]][['xyz']] <- cbind(
+					as.numeric(strsplit(shapes[[lnum]][['cx']], ',')[[1]]), 
+					as.numeric(strsplit(shapes[[lnum]][['cy']], ',')[[1]]), 
+					as.numeric(strsplit(shapes[[lnum]][['cz']], ',')[[1]])
+				)
+			}else{
+				shapes[[lnum]][['xyz']] <- matrix(unlist(shapes[[lnum]][c('cx', 'cy', 'cz')]), nrow=1, ncol=3)
+			}
 		}
 
-		if(type == 'circle' && grepl(',', svg.list[[lnum]][['cx']])){
-			svg.list[[lnum]][['xyz']] <- cbind(
-				as.numeric(strsplit(svg.list[[lnum]][['cx']], ',')[[1]]), 
-				as.numeric(strsplit(svg.list[[lnum]][['cx']], ',')[[1]]), 
-				as.numeric(strsplit(svg.list[[lnum]][['cx']], ',')[[1]])
-			)
+		if(type %in% c('line', 'arrow')){
+			if(grepl(',', shapes[[lnum]][['x1']])){
+				shapes[[lnum]][['xyz']] <- cbind(
+					as.numeric(strsplit(shapes[[lnum]][['x1']], ',')[[1]]), 
+					as.numeric(strsplit(shapes[[lnum]][['y1']], ',')[[1]]), 
+					as.numeric(strsplit(shapes[[lnum]][['z1']], ',')[[1]]),
+					as.numeric(strsplit(shapes[[lnum]][['x2']], ',')[[1]]), 
+					as.numeric(strsplit(shapes[[lnum]][['y2']], ',')[[1]]), 
+					as.numeric(strsplit(shapes[[lnum]][['z2']], ',')[[1]])
+				)
+			}else{
+				shapes[[lnum]][['xyz']] <- matrix(unlist(shapes[[lnum]][c('x1', 'y1', 'z1', 'x2', 'y2', 'z2')]), nrow=1, ncol=6)
+			}
 		}
-
-		if(type %in% c('line', 'arrow') && grepl(',', svg.list[[lnum]][['x1']])){
-			svg.list[[lnum]][['xyz']] <- cbind(
-				as.numeric(strsplit(svg.list[[lnum]][['x1']], ',')[[1]]), 
-				as.numeric(strsplit(svg.list[[lnum]][['y1']], ',')[[1]]), 
-				as.numeric(strsplit(svg.list[[lnum]][['z1']], ',')[[1]]),
-				as.numeric(strsplit(svg.list[[lnum]][['x2']], ',')[[1]]), 
-				as.numeric(strsplit(svg.list[[lnum]][['y2']], ',')[[1]]), 
-				as.numeric(strsplit(svg.list[[lnum]][['z2']], ',')[[1]])
-			)
-		}
-
-		# Save z.index
-		if(!is.null(svg.list[[lnum]][['z-index']])) z.index[lnum] <- svg.list[[lnum]][['z-index']]
-	}
 		
+		for(name_split in split_comma){
+			if(is.null(shapes[[lnum]][[name_split]])) next
+			if(!grepl(',', shapes[[lnum]][[name_split]])) next
+			shapes[[lnum]][[name_split]] <- strsplit(shapes[[lnum]][[name_split]], ',')[[1]]
+			if(!is.na(suppressWarnings(as.numeric(shapes[[lnum]][[name_split]][1])))) shapes[[lnum]][[name_split]] <- as.numeric(shapes[[lnum]][[name_split]])
+		}
+		
+		# Save z.index
+		if(!is.null(shapes[[lnum]][['z-index']])) z.index[lnum] <- shapes[[lnum]][['z-index']]
+	}
+	
+	# Copy pathC points to pathC
+	for(lnum in 1:length(svg_vec)){
+		
+		if(shapes[[lnum]]$type != 'pathC') next
+		
+		# Get point indices
+		point_indices <- point_idx[shapes[[lnum]][['d']]]
+
+		# Get coordinates of first point
+		first_point <- shapes[[point_indices[1]]][['xyz']]
+
+		# Create array for points
+		shapes[[lnum]][['xyz']] <- array(NA, dim=c(length(point_indices), rev(dim(first_point))))
+
+		# Fill array
+		for(i in 1:length(point_indices)) shapes[[lnum]][['xyz']][i, , ] <- t(shapes[[point_indices[i]]][['xyz']])
+	}	
+
 	# Check results
-	#for(lnum in 1:length(svg_vec)) cat(paste0(names(svg.list[[lnum]]), '=', svg.list[[lnum]]), '\n')
+	#for(lnum in 1:length(svg_vec)) cat(paste0(names(shapes[[lnum]]), '=', shapes[[lnum]]), '\n')
 
 	list(
-		'svg.list'=svg.list,
+		'params'=params,
+		'shapes'=shapes,
 		'z.index'=z.index
 	)
 }
--- R/svg2img.R
+++
@@ -1,6 +0,0 @@
-svg2img <- function(svg.file, img.file){
-	## Converts an svg file to an image or set of images
-	
-	# Read svg file
-	svg_list <- readSVG(svg.file)
-}
