b74b7c7e824862edadcc4aecdd3d0c4fe14c98b9
Aaron Olsen <aaronolsen@users.noreply.github.com>
2016-12-13 00:42:08 -0500

    Function updated

--- R/webColor.R
+++ R/webColor.R
@@ -2,6 +2,11 @@ webColor <- function(col){
 	
 	if(length(col) > 1) return(sapply(col, webColor))
 	
+	if(grepl(',', col)){
+		col_split <- strsplit(col, ',')[[1]]
+		return(paste(webColor(col_split), collapse=','))
+	}
+	
 	if(substr(col, 1, 1) == '#' && nchar(col) > 7) col <- paste0('rgb(', paste(c(col2rgb(col)), collapse=','), ')')
 
 	col
