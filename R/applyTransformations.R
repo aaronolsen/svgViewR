45034ec6bd98c1d6f356da1ecbb83c660e376e60
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-03-06 22:34:52 -0500

    Updated to increase speed a bit

--- R/applyTransformations.R
+++ R/applyTransformations.R
@@ -30,60 +30,70 @@ applyTransformations <- function(read_html){
 	# Set total number of iterations as maximum of both
 	n_iter <- max(c(shapes_n_iter, rotate_n_iter, translate_n_iter))
 	
-	# Expand xyz values to new number of iterations
-	for(lnum in 1:length(shapes)){
+	if(length(tform[['translate']]) > 0){translate <- TRUE}else{translate <- FALSE}
+	if(length(tform[['rotate']]) > 0){rotate <- TRUE}else{rotate <- FALSE}
 
+	# Add 0 rows to match number of iterations
+	if(translate) if(nrow(tform[['translate']]) < n_iter) tform[['translate']] <- rbind(tform[['translate']], matrix(0, nrow=n_iter-nrow(tform[['translate']]), ncol=3))
+
+	#t1 <- proc.time()
+
+	for(lnum in 1:length(shapes)){
+		
 		if(is.null(shapes[[lnum]][['xyz']])) next
 		if(shapes[[lnum]][['type']] == 'path') next
-		xyz <- shapes[[lnum]][['xyz']]
 
-		if(length(dim(xyz)) == 2){
-			if(nrow(xyz) == 1){
-				shapes[[lnum]][['xyz']] <- matrix(xyz, nrow=n_iter, ncol=ncol(xyz), byrow=TRUE)
+		xyz <- shapes[[lnum]][['xyz']]
+		xyz_dim <- dim(shapes[[lnum]][['xyz']])
+
+		# Expand xyz values to new number of iterations
+		if(length(xyz_dim) == 2){
+			if(xyz_dim[1] == 1){
+				shapes[[lnum]][['xyz']] <- matrix(xyz, nrow=n_iter, ncol=xyz_dim[2], byrow=TRUE)
 			}else{
-				for(i in 1:(round(n_iter/nrow(xyz))-1)) xyz <- rbind(xyz, xyz)
+				for(i in 1:(round(n_iter/xyz_dim[1])-1)) xyz <- rbind(xyz, xyz)
 				shapes[[lnum]][['xyz']] <- xyz
 			}
-		}else if(length(dim(xyz)) == 3){
-			if(dim(xyz)[3] == 1){
-				shapes[[lnum]][['xyz']] <- array(xyz, dim=c(dim(xyz)[1:2], n_iter))
+		}else if(length(xyz_dim) == 3){
+			if(xyz_dim[3] == 1){
+				shapes[[lnum]][['xyz']] <- array(xyz, dim=c(xyz_dim[1:2], n_iter))
 			}else{
-				shapes[[lnum]][['xyz']] <- array(xyz, dim=c(dim(xyz)[1:2], n_iter))
+				shapes[[lnum]][['xyz']] <- array(xyz, dim=c(xyz_dim[1:2], n_iter))
 			}
 		}
-	}
 
-	# For each iteration
-	for(iter in 1:n_iter){
+		# Get new dimensions
+		xyz_dim <- dim(shapes[[lnum]][['xyz']])
 
-		for(lnum in 1:length(shapes)){
-
-			if(is.null(shapes[[lnum]][['xyz']])) next
-			if(shapes[[lnum]][['type']] == 'path') next
-
-			xyz <- shapes[[lnum]][['xyz']]
-
-			# For each transformation
-			if(length(tform[['rotate']]) > 0 && iter <= rotate_n_iter){
-
-				if(length(dim(xyz)) == 2){
+		# Apply rotations -- this part is slow for a lot of iterations, can't figure out how to do it without for loops
+		# will probably need to use compiled code
+		if(rotate){
+			if(length(xyz_dim) == 2){
+				for(iter in 1:rotate_n_iter){
 					r <- matrix(shapes[[lnum]][['xyz']][iter, ], ncol=3, byrow=TRUE) %*% tform[['rotate']][, , iter]
-					shapes[[lnum]][['xyz']][iter, ] <- c(t(r))
-				}else if(length(dim(xyz)) == 3){
+					shapes[[lnum]][['xyz']][iter, ] <- t(r)
+				}
+			}else if(length(xyz_dim) == 3){
+				for(iter in 1:rotate_n_iter){
 					shapes[[lnum]][['xyz']][, , iter] <- shapes[[lnum]][['xyz']][, , iter] %*% tform[['rotate']][, , iter]
 				}
 			}
-			if(length(tform[['translate']]) > 0 && iter <= translate_n_iter){
-				
-				if(length(dim(xyz)) == 2){
-					shapes[[lnum]][['xyz']][iter, ] <- shapes[[lnum]][['xyz']][iter, ] + matrix(tform[['translate']][iter, ], nrow=1, ncol=ncol(xyz))
-				}else if(length(dim(xyz)) == 3){
-					shapes[[lnum]][['xyz']][, , iter] <- shapes[[lnum]][['xyz']][, , iter] + matrix(tform[['translate']][iter, ], nrow=nrow(xyz), ncol=ncol(xyz), byrow=TRUE)
-				}
+		}
+
+		# Apply translations
+		if(translate){
+			if(length(xyz_dim) == 2){
+				shapes[[lnum]][['xyz']] <- shapes[[lnum]][['xyz']] + matrix(tform[['translate']], nrow=xyz_dim[1], ncol=xyz_dim[2], byrow=TRUE)
+			}else if(length(xyz_dim) == 3){
+				tarr <- array(apply(tform[['translate']], 1, 'matrix', nrow=xyz_dim[1], ncol=xyz_dim[2], byrow=TRUE), dim=xyz_dim)
+				shapes[[lnum]][['xyz']] <- shapes[[lnum]][['xyz']] + tarr
 			}
 		}
 	}
 	
+	#t2 <- proc.time()
+	#print(t2-t1)
+
 	rlist <- list(
 		'params'=params,
 		'shapes'=shapes,
