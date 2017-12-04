d46f06d58fcf501699f9386b7cd4f00817846862
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-12-03 22:14:39 -0500

    Added option for exact limits

--- R/svgviewr_ranges.R
+++ R/svgviewr_ranges.R
@@ -1,11 +1,16 @@
-svgviewr_ranges <- function(x, ranges, tick.num){
+svgviewr_ranges <- function(x, ranges, tick.num, lim.exact=FALSE){
 	
 	if(!is.null(x) && is.array(x)) ranges <- apply(x, 2, 'range', na.rm=TRUE)
 	if(!is.null(x) && is.matrix(x)) ranges <- apply(x, 2, 'range', na.rm=TRUE)
 	
-	# Round to 1-2 significant digits
-	rangesr <- round(ranges, -log(abs(ranges), base=10)+1)
-
+	if(lim.exact){
+		# Trying exact - tick labels may not be correct yet
+		rangesr <- ranges
+	}else{
+		# Round to 1-2 significant digits
+		rangesr <- round(ranges, -log(abs(ranges), base=10)+1)
+	}
+	
 	# Make variables for limits
 	xlim <- rangesr[, 1]
 	ylim <- rangesr[, 2]
@@ -17,9 +22,15 @@ svgviewr_ranges <- function(x, ranges, tick.num){
 	max_range <- max(ranges_diff)
 	grid_by <- max_range / tick.num
 	
-	xlim <- c(rangesr[1,1]-grid_by, rangesr[1,1]+grid_by*ceiling(ranges_diff[1] / grid_by) + grid_by)
-	ylim <- c(rangesr[1,2]-grid_by, rangesr[1,2]+grid_by*ceiling(ranges_diff[2] / grid_by) + grid_by)
-	zlim <- c(rangesr[1,3]-grid_by, rangesr[1,3]+grid_by*ceiling(ranges_diff[3] / grid_by) + grid_by)
+	if(lim.exact){
+		xlim <- range(rangesr[,1])
+		ylim <- range(rangesr[,2])
+		zlim <- range(rangesr[,3])
+	}else{
+		xlim <- c(rangesr[1,1]-grid_by, rangesr[1,1]+grid_by*ceiling(ranges_diff[1] / grid_by) + grid_by)
+		ylim <- c(rangesr[1,2]-grid_by, rangesr[1,2]+grid_by*ceiling(ranges_diff[2] / grid_by) + grid_by)
+		zlim <- c(rangesr[1,3]-grid_by, rangesr[1,3]+grid_by*ceiling(ranges_diff[3] / grid_by) + grid_by)
+	}
 	
 	return(list(
 		'xlim' = xlim,
