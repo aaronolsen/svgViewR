43b887f315ecf9a0983c9901ad1d50d3dd0c19f5
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-12-04 23:02:25 -0500

    Added WebGL output option

--- R/svg.arrows.R
+++ R/svg.arrows.R
@@ -1,18 +1,21 @@
-svg.arrows <- function(x, y=NULL, col="black", z.index=0, layer="", 
+svg.arrows <- function(x, y=NULL, name="arrow", col="black", z.index=0, layer="", 
 	label="", lwd=1, len='auto', angle=0.4, opacity=1, file=NULL){
 
 	#print(names(file))
 	#print(names(svg_glo$con))
 	#cat('--------\n')
 	
-	# If file is null, set current connection
-	if(is.null(file)){
+	if('webgl' != getOption("svgviewr_glo_type")){
 
-		# Give error if svg.new has not been called
-		if(is.null(getOption("svg_glo_con"))) stop("svg.new has not been called yet.")
+		# If file is null, set current connection
+		if(is.null(file)){
 
-		# Get current connection
-		file <- getOption("svg_glo_con")
+			# Give error if svg.new has not been called
+			if(is.null(getOption("svg_glo_con"))) stop("svg.new has not been called yet.")
+
+			# Get current connection
+			file <- getOption("svg_glo_con")
+		}
 	}
 
 	# Determine arrow length from line length
@@ -21,10 +24,26 @@ svg.arrows <- function(x, y=NULL, col="black", z.index=0, layer="",
 		if(is.array(x) && length(dim(x)) > 2) len <- max(sqrt(colSums((x[1, , ]-x[dim(x)[1], , ])^2)))*0.05
 	}
 
-	# Plot line
-	svgviewr.lines(x, file=file, y=y, col=col, z.index=z.index, layer=layer, 
-		label=label, lwd=lwd, opacity=opacity, arrow.len=len, arrow.angle=angle, 
-		tag.name='arrow')
+	if('webgl' == getOption("svgviewr_glo_type")){
+
+		## Add objects to svgViewR environment
+		# Get viewer environment
+		env <- as.environment(getOption("svgviewr_glo_env"))
+		
+		# Get arrow length
+		arrow_len = sqrt(sum((x[2,]-x[1,])^2))
+
+		# Add arrow
+		env$svgviewr_env$arrow[[length(svgviewr_env$arrow)+1]] <- list('type'='arrow', 
+			'names'=name, 'origin'=x[1,], 'dir'=uvector_svg(x[2,]-x[1,]), 'length'=arrow_len, 
+			'len'=len*2.5, 'col'=webColor(col), 'lwd'=lwd)
+	}else{
+
+		# Plot line
+		svgviewr.lines(x, file=file, y=y, col=col, z.index=z.index, layer=layer, 
+			label=label, lwd=lwd, opacity=opacity, arrow.len=len, arrow.angle=angle, 
+			tag.name='arrow')
+	}
 
 	# Suppress return of value in console
 	ret = NULL
