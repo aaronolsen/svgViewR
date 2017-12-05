2a5987ad5b99f195cfb90887d0d6475351e6f985
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-12-04 22:13:56 -0500

    Added more input parameters

--- R/svg.frame.R
+++ R/svg.frame.R
@@ -1,12 +1,12 @@
-svg.frame <- function(x = NULL, ranges = NULL, grid.lwd = 1, tick.lwd = 1, 
-	tick.num = 10, tick.label.size = 0.13, axis.label.size = 0.14, tick.label.opacity = 1, 
-	axis.label.opacity = 1, grid.opacity = 0.1, z.index=0, lim.exact=FALSE, file=NULL){
+svg.frame <- function(x = NULL, ranges = NULL, grid.lwd = 1, tick.lwd = 1, tick.num = 10, 
+	tick.label.size = 0.13, axis.label.size = 0.14, tick.label.opacity = 1, 
+	axis.label.opacity = 1, grid.opacity = 0.1, col = 'black', z.index=0, lim.exact=FALSE, file=NULL){
 
 	svg_box <- svg.box(x=x, ranges=ranges, sides=1:3, grid.lwd=grid.lwd, tick.axes=c(2,3,2), 
 		tick.labels=c(2,3,2), tick.lwd=tick.lwd, tick.num=tick.num, tick.label.size=tick.label.size, 
 		tick.label.opacity=tick.label.opacity, axis.label.opacity=axis.label.opacity, 
-		axis.label.size=axis.label.size, grid.opacity=grid.opacity, z.index=z.index, lim.exact=lim.exact, 
-		file=file)
+		axis.label.size=axis.label.size, grid.opacity=grid.opacity, col=col, z.index=z.index, 
+		lim.exact=lim.exact, file=file)
 	
 	list('lim'=svg_box$lim)
 }
