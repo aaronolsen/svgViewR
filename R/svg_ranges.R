4942abf2f27ed87ba552cc8e5b94274e4b4a172e
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-12-06 23:23:00 -0500

    Added range calculation for updated states

--- R/svg_ranges.R
+++ R/svg_ranges.R
@@ -11,55 +11,111 @@ svg_ranges <- function(x = NULL){
 
 			# Get all svg objects with xyz limits
 			svg_obj_names <- names(env$svgviewr_env)[!names(env$svgviewr_env) %in% c('names', 'js_var', 'tm')]
-			
-			# Start svg_range matrix
-			x <- matrix(NA, nrow=0, 3)
 
-			# Get ranges for each object
-			for(svg_obj_name in svg_obj_names){
-
-				if(svg_obj_name == 'mesh'){
+			# Get static ranges
+			ranges <- matrix(NA, nrow=0, 3)
+   			
+			for(oi in 1:length(env$svgviewr_env$ref$type)){
 				
-					for(mesh_num in 1:length(env$svgviewr_env[[svg_obj_name]])){
+				# Get object
+				obj <- env$svgviewr_env[[env$svgviewr_env$ref$type[oi]]][[env$svgviewr_env$ref$num[oi]]]
+				
+				# If transformed via transformation, skip static ranges
+				if(!is.null(obj$tm)) next
 
-						# Get mesh
-						mesh <- env$svgviewr_env[[svg_obj_name]][[mesh_num]]
+				if(env$svgviewr_env$ref$type[oi] == 'mesh'){
+
+					if(is.null(obj[['lim']])){
 
 						# Read source file
-						mesh_json <- fromJSON(paste(suppressWarnings(readLines(mesh$file)), collapse=""))
-						
+						obj_json <- fromJSON(paste(suppressWarnings(readLines(obj$file)), collapse=""))
+				
 						# Get number of vertices
-						num_vertices <- length(mesh_json$vertices)
+						num_vertices <- length(obj_json$vertices)
 
 						# Get xyz limits
-						vertex_ranges <- cbind(range(mesh_json$vertices[seq(1, num_vertices-2, by=3)], na.rm=TRUE),
-							range(mesh_json$vertices[seq(2, num_vertices-1, by=3)], na.rm=TRUE),
-							range(mesh_json$vertices[seq(3, num_vertices, by=3)], na.rm=TRUE))
+						obj_ranges <- cbind(range(obj_json$vertices[seq(1, num_vertices-2, by=3)], na.rm=TRUE),
+							range(obj_json$vertices[seq(2, num_vertices-1, by=3)], na.rm=TRUE),
+							range(obj_json$vertices[seq(3, num_vertices, by=3)], na.rm=TRUE))
 						
-						# Add ranges to environment object
-						env$svgviewr_env[[svg_obj_name]][[mesh_num]][['range']] <- vertex_ranges
+						# Get corners
+						corners <- limits2corners(obj_ranges)
 
-						x <- rbind(x, vertex_ranges)
+						# Add to object
+						env$svgviewr_env[[env$svgviewr_env$ref$type[oi]]][[env$svgviewr_env$ref$num[oi]]][['lim']] <- obj_ranges
+						env$svgviewr_env[[env$svgviewr_env$ref$type[oi]]][[env$svgviewr_env$ref$num[oi]]][['corners']] <- corners
+
+					}else{
+
+						# Limits determined when object was loaded
+						corners <- obj[['corners']]
 					}
+				}else if(env$svgviewr_env$ref$type[oi] == 'sphere'){
+				
+					# Get limits
+					if(is.null(obj$x_tm)){
+						lim <- rbind(obj$x + obj$radius*c(1,1,1), obj$x - obj$radius*c(1,1,1))
+					}else{
+						mat_lim <- apply(matrix(unlist(obj$x_tm), ncol = 3, byrow = TRUE), 2, 'range', na.rm=TRUE)
+						lim <- rbind(mat_lim[1,] + obj$radius*c(1,1,1), mat_lim[1,] - obj$radius*c(1,1,1), 
+							mat_lim[2,] + obj$radius*c(1,1,1), mat_lim[2,] - obj$radius*c(1,1,1))
+						lim <- apply(lim, 2, 'range', na.rm=TRUE)
+					}
+					
+					# Get corners
+					corners <- lim2corners(lim)
+
+				}else if(env$svgviewr_env$ref$type[oi] == 'line'){
+
+					# Get limits
+					if(is.null(obj$x_tm)){
+						lim <- apply(t(obj$x), 2, 'range', na.rm=TRUE)
+					}else{
+						lim <- apply(t(matrix(unlist(obj$x_tm), nrow = 3, byrow = FALSE)), 2, 'range', na.rm=TRUE)
+					}
+
+					# Get corners
+					corners <- lim2corners(lim)
 				}
 
-				if(svg_obj_name == 'arrow'){
+				# Add ranges
+				ranges <- rbind(ranges, corners)
+			}
+
+			# Apply each animation
+			if(!is.null(env$svgviewr_env$tm)){
+
+				# For each animation
+				for(animation in names(env$svgviewr_env$tm)){
+			
+					# For each named set
+					for(applyto in names(env$svgviewr_env$tm[[animation]])){
 				
-					for(arrow_num in 1:length(env$svgviewr_env[[svg_obj_name]])){
+						# Get transformations
+						tmat <- env$svgviewr_env$tm[[animation]][[applyto]][['tmat']]
+					
+						# Find matching names in ref
+						names_matches <- which(applyto == env$svgviewr_env$ref$names)
+						obj_type_matches <- env$svgviewr_env$ref$type[names_matches]
+						obj_num_matches <- env$svgviewr_env$ref$num[names_matches]
 
-						# Get arrow
-						arrow <- env$svgviewr_env[[svg_obj_name]][[arrow_num]]
+						# Get range for all matching objects
+						for(mn in 1:length(obj_type_matches)){
+					
+							if(!obj_type_matches[mn] %in% c('mesh')) next
 
-						# Get start and end point of arrow
-						arrow_ranges <- rbind(arrow$origin, arrow$origin + arrow$length*arrow$dir)
+							# Get corners
+							obj_range <- env$svgviewr_env[[obj_type_matches[mn]]][[obj_num_matches[mn]]][['corners']]
+						
+							# Apply transformations to each and get ranges
+							obj_tm <- apply_transform_svg(obj_range, tmat)
 
-						x <- rbind(x, arrow_ranges)
+							# Add range
+							ranges <- rbind(ranges, apply(obj_tm, 2, 'range', na.rm=TRUE))
+						}
 					}
 				}
 			}
-
-			# Apply any transformations to ranges for each element
-			# Replace any previous
 
 		}else{
 
@@ -69,7 +125,7 @@ svg_ranges <- function(x = NULL){
 	}
 
 	# Find ranges (should work for matrices and arrays)
-	ranges <- apply(x, 2, 'range', na.rm=TRUE)
+	ranges <- apply(ranges, 2, 'range', na.rm=TRUE)
 
 	ranges
 }
