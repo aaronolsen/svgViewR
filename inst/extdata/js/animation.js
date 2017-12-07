9ab3eeb5dfa661062ed0b161d4c107b18cb38385
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-12-06 23:19:59 -0500

    Index which objects to apply animation states to

--- inst/extdata/js/animation.js
+++ inst/extdata/js/animation.js
@@ -9,7 +9,20 @@ function load_animation( str ) {
 	// Get time units
 	animation.units = 'msec';
 	if(JSON.parse(str).units == 'sec' || JSON.parse(str).units == 's') animation.units = 'sec';
-	
+
+	// Set animate	
+	if(animation.tracks.length == 0){
+
+		// Get animation times/length if only update_obj is used (no transformations)
+		// if(update_obj.num.length > 0){
+		// if(update_obj.type[0] == 'sphere') spheres[update_obj.num[0]].x_tm.length
+
+		animate = false;
+		return;
+	}
+
+	animate = true;
+
 	// Get times
 	animation.times = getUniqueTimes( animation );
 	
@@ -27,27 +40,41 @@ function load_animation( str ) {
 
 function getUniqueTimes( animation ){
 
-	var j, keys_length;
+	var i, j, keys_length;
 	var times = [];
-	var apply_to;
+	var num, apply_to, apply_type;
 	
 	var factor = 1;
 	if(animation.units == 'sec') factor = 1000;
 	
 	var tracks_length = animation.tracks.length;
-	for (var i = 0; i < tracks_length; i++){
+	for (i = 0; i < tracks_length; i++){
 
 		// Get index of apply
 		apply_to = animation.tracks[i].apply;
+
+		animation.tracks[i].num = new Array();
+		animation.tracks[i].type = new Array();
+
+		//// Find objects with matching name
+		for(j = 0; j < svg_obj.mesh.length; j++){
+			if(apply_to == svg_obj.mesh[j].name){
+				animation.tracks[i].num.push(j);
+				animation.tracks[i].type.push('mesh');
+			}
+		}
 		
 		// Find corresponding mesh
 		for(j = 0; j < svg_obj.mesh.length; j++){
-			if(animation.tracks[i].apply == svg_obj.mesh[j].name){
+			if(apply_to == svg_obj.mesh[j].name){
 				animation.tracks[i].apply_idx = j;
 				break;
 			}
 		}
-
+		
+		//alert(animation.tracks[i].num + '\n' + animation.tracks[i].type);
+		
+		// Add times
 		keys_length = animation.tracks[i].keys.length;
 		for (j = 0; j < keys_length; j++){
 			times.push(animation.tracks[i].keys[j].time * factor * (1/play_speed));
