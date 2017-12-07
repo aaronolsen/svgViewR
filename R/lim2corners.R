306f9db26617edb6c9e95094f2517db2877b5642
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-12-06 23:20:15 -0500

    Created function to return box corners from xyz-limits

---
+++ R/lim2corners.R
@@ -0,0 +1,16 @@
+lim2corners <- function(lim){
+	
+	# Set corners
+	corners <- rbind(
+		c(lim[1,1], lim[1,2], lim[1,3]),
+		c(lim[1,1], lim[2,2], lim[1,3]),
+		c(lim[1,1], lim[2,2], lim[2,3]),
+		c(lim[1,1], lim[1,2], lim[2,3]),
+		c(lim[2,1], lim[1,2], lim[1,3]),
+		c(lim[2,1], lim[2,2], lim[1,3]),
+		c(lim[2,1], lim[2,2], lim[2,3]),
+		c(lim[2,1], lim[1,2], lim[2,3])
+	)
+	
+	corners
+}
