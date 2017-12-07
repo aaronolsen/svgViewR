488f04812cef45a4db769281eb921b516ab87c44
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-12-06 23:21:20 -0500

    Created new function to draw sphere for WebGL output

---
+++ R/svg.spheres.R
@@ -0,0 +1,27 @@
+svg.spheres <- function(x, radius = 1, col = 'black', name = 'sphere', wseg = 16, hseg = 16){
+
+	# Make sure that type is webgl
+	if('webgl' != getOption("svgviewr_glo_type")) stop("Sphere drawing is currently only available with webgl svgViewR output.")
+
+	# Get viewer environment
+	env <- as.environment(getOption("svgviewr_glo_env"))
+
+	# If vector, make into matrix
+	if(is.vector(x)) x <- matrix(x, nrow=1, ncol=length(x))
+
+	# Add object
+	for(i in 1:nrow(x)){
+
+		add_at <- length(svgviewr_env$sphere)+1
+		env$svgviewr_env$sphere[[add_at]] <- list('type'='sphere', 'name'=name, 'x'=setNames(x[i,], NULL), 
+			'radius'=radius, 'col'=webColor(col), 'wseg'=16, 'hseg'=16)
+
+		# Add object reference data
+		env$svgviewr_env$ref$names <- c(env$svgviewr_env$ref$names, name)
+		env$svgviewr_env$ref$num <- c(env$svgviewr_env$ref$num, add_at)
+		env$svgviewr_env$ref$type <- c(env$svgviewr_env$ref$type, 'sphere')
+	}
+
+	# Suppress return of value in console
+	ret = NULL
+}
