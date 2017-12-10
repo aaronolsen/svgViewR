4c6fe20157b714ac9c2b92609da2fb7f9af6020e
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-12-09 19:30:31 -0500

    Added option for WebGL output

--- R/svg.text.R
+++ R/svg.text.R
@@ -1,23 +1,50 @@
-svg.text <- function(x, y = NULL, labels = NULL, layer="", 
-	font.size = 12, col = "black", text.anchor = "middle", font.family = "Arial", 
+svg.text <- function(x, y = NULL, labels = NULL, layer="", name="text", 
+	font.size = 12, size = 1, col = "black", text.anchor = "middle", font.family = "Arial", 
 	opacity = 1, font.style = "", font.weight = "", letter.spacing = 0, writing.mode = "", 
 	glyph.orientation.vertical = "", z.index=0, file=NULL){
 
-	# If file is null, set current connection
-	if(is.null(file)){
+	if('webgl' != getOption("svgviewr_glo_type")){
 
-		# Give error if svg.new has not been called
-		if(is.null(getOption("svg_glo_con"))) stop("svg.new has not been called yet.")
+		# If file is null, set current connection
+		if(is.null(file)){
 
-		# Get current connection
-		file <- getOption("svg_glo_con")
+			# Give error if svg.new has not been called
+			if(is.null(getOption("svg_glo_con"))) stop("svg.new has not been called yet.")
+
+			# Get current connection
+			file <- getOption("svg_glo_con")
+		}
+
+		svgviewr.text(x=x, file=file, y=y, labels=labels, layer=layer, 
+			font.size=font.size, col=col, text.anchor=text.anchor, font.family=font.family, 
+			opacity=opacity, font.style=font.style, font.weight=font.weight, 
+			letter.spacing=letter.spacing, writing.mode=writing.mode, 
+			glyph.orientation.vertical=glyph.orientation.vertical, z.index=z.index)
+
+	}else{
+
+		# Get viewer environment
+		env <- as.environment(getOption("svgviewr_glo_env"))
+
+		# Convert to matrix if vector
+		if(is.vector(x)) x <- matrix(x, nrow=1, )
+
+		if(length(dim(x)) == 2){
+
+			for(i in 1:nrow(x)){
+
+				# Add text to environment
+				add_at <- length(svgviewr_env$text)+1
+				env$svgviewr_env$text[[add_at]] <- list('type'='text', 'labels'=as.character(labels[i]), 
+					'name'=name, 'x'=x[i,], 'col'=webColor(col), 'size'=size)
+
+				# Add object reference data
+				env$svgviewr_env$ref$names <- c(env$svgviewr_env$ref$names, name)
+				env$svgviewr_env$ref$num <- c(env$svgviewr_env$ref$num, add_at)
+				env$svgviewr_env$ref$type <- c(env$svgviewr_env$ref$type, 'text')
+			}
+		}
 	}
-
-	svgviewr.text(x=x, file=file, y=y, labels=labels, layer=layer, 
-		font.size=font.size, col=col, text.anchor=text.anchor, font.family=font.family, 
-		opacity=opacity, font.style=font.style, font.weight=font.weight, 
-		letter.spacing=letter.spacing, writing.mode=writing.mode, 
-		glyph.orientation.vertical=glyph.orientation.vertical, z.index=z.index)
 
 	# Suppress return of value in console
 	ret = NULL
