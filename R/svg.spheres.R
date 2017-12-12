fd8f81912beea179a65533374e321695b6745fff
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-12-11 22:13:00 -0500

    Minor edits

--- R/svg.spheres.R
+++ R/svg.spheres.R
@@ -3,24 +3,30 @@ svg.spheres <- function(x, radius = 1, col = 'black', name = 'sphere', wseg = 16
 	# Make sure that type is webgl
 	if('webgl' != getOption("svgviewr_glo_type")) stop("Sphere drawing is currently only available with webgl svgViewR output.")
 
-	# Get viewer environment
-	env <- as.environment(getOption("svgviewr_glo_env"))
+	if('webgl' == getOption("svgviewr_glo_type")){
 
-	# If vector, make into matrix
-	if(is.vector(x)) x <- matrix(x, nrow=1)
+		# Get viewer environment
+		env <- as.environment(getOption("svgviewr_glo_env"))
 
-	# Add object
-	for(i in 1:nrow(x)){
+		# If vector, make into matrix
+		if(is.vector(x)) x <- matrix(x, nrow=1)
 
-		add_at <- length(svgviewr_env$sphere)+1
-		env$svgviewr_env$sphere[[add_at]] <- list('type'='sphere', 'name'=name, 'x'=setNames(x[i,], NULL), 
-			'radius'=radius, 'col'=webColor(col),  
-			'wseg'=16, 'hseg'=16)
+		# Add object
+		for(i in 1:nrow(x)){
 
-		# Add object reference data
-		env$svgviewr_env$ref$names <- c(env$svgviewr_env$ref$names, name)
-		env$svgviewr_env$ref$num <- c(env$svgviewr_env$ref$num, add_at)
-		env$svgviewr_env$ref$type <- c(env$svgviewr_env$ref$type, 'sphere')
+			add_at <- length(svgviewr_env$sphere)+1
+			env$svgviewr_env$sphere[[add_at]] <- list('type'='sphere', 'name'=name, 'x'=setNames(x[i,], NULL), 
+				'radius'=radius, 'col'=webColor(col),  
+				'wseg'=16, 'hseg'=16)
+
+			# Add object reference data
+			env$svgviewr_env$ref$names <- c(env$svgviewr_env$ref$names, name)
+			env$svgviewr_env$ref$num <- c(env$svgviewr_env$ref$num, add_at)
+			env$svgviewr_env$ref$type <- c(env$svgviewr_env$ref$type, 'sphere')
+		}
+
+	}else{
+		
 	}
 
 	# Suppress return of value in console
