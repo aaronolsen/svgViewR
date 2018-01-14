282ff90dee30be178e962b23fa91420b8924001b
Aaron Olsen <aaronolsen@users.noreply.github.com>
2018-01-13 23:37:33 -0500

    Added ignore for NA values

--- R/svg_box_lim.R
+++ R/svg_box_lim.R
@@ -16,13 +16,13 @@ svg_box_lim <- function(ranges, tick.num, lim.exact=FALSE){
 	ranges_diff <- rangesr[2, ] - rangesr[1, ]
 
 	# Set increment for grid squares
-	max_diff <- max(ranges_diff)
+	max_diff <- max(ranges_diff, na.rm=TRUE)
 	grid_by <- max_diff / tick.num
 	
 	if(lim.exact){
-		xlim <- range(rangesr[,1])
-		ylim <- range(rangesr[,2])
-		zlim <- range(rangesr[,3])
+		xlim <- range(rangesr[,1], na.rm=TRUE)
+		ylim <- range(rangesr[,2], na.rm=TRUE)
+		zlim <- range(rangesr[,3], na.rm=TRUE)
 	}else{
 		xlim <- c(rangesr[1,1]-grid_by, rangesr[1,1]+grid_by*ceiling(ranges_diff[1] / grid_by) + grid_by)
 		ylim <- c(rangesr[1,2]-grid_by, rangesr[1,2]+grid_by*ceiling(ranges_diff[2] / grid_by) + grid_by)
@@ -35,7 +35,7 @@ svg_box_lim <- function(ranges, tick.num, lim.exact=FALSE){
 		'zlim' = zlim,
 		'lim' = cbind(xlim, ylim, zlim),
 		'max_diff' = max_diff,
-		'med_diff' = median(ranges_diff),
+		'med_diff' = median(ranges_diff, na.rm=TRUE),
 		'grid_by' = grid_by
 		)
 	)
