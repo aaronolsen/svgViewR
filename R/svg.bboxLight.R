e2ae4727e42176723e7af5f980a271c3677fc127
Aaron Olsen <aaronolsen@users.noreply.github.com>
2018-01-17 14:12:42 -0500

    Created new function for user to add lights to corner of bounding box

---
+++ R/svg.bboxLight.R
@@ -0,0 +1,29 @@
+svg.bboxLight <- function(x = c(1,1,1), col = '#FFFFDD', intensity = 1, distance = 3, 
+	hidden = TRUE){
+
+	# Make sure that type is webgl
+	if('svg' == getOption("svgviewr_glo_type")) stop("Lighting is currently only available with webgl svgViewR output.")
+
+	# Get viewer environment
+	env <- as.environment(getOption("svgviewr_glo_env"))
+
+	# If is vector, convert to matrix
+	if(is.vector(x)) x <- matrix(x, nrow=1, ncol=3)
+
+	#
+	for(i in 1:nrow(x)){
+
+		# Add to environment
+		add_at <- length(svgviewr_env$bboxLight)+1
+
+		# Add vertices
+		env$svgviewr_env$bboxLight[[add_at]] <- list()
+		env$svgviewr_env$bboxLight[[add_at]]$x <- x[i,]
+		env$svgviewr_env$bboxLight[[add_at]]$col <- webColor(col)
+		env$svgviewr_env$bboxLight[[add_at]]$intensity <- intensity
+		env$svgviewr_env$bboxLight[[add_at]]$distance <- distance
+		env$svgviewr_env$bboxLight[[add_at]]$hidden <- hidden
+	}
+
+	NULL
+}
