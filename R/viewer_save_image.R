55292b94c4ac9fb6275131e11dc6e68736053c51
Aaron Olsen <aaronolsen@users.noreply.github.com>
2018-02-28 16:27:08 -0500

    Created new function to save image from URL/URI

---
+++ R/viewer_save_image.R
@@ -0,0 +1,15 @@
+viewer_save_image <- function(from, to){
+
+	# Decode 'to'
+	to_decode <- URLdecode(to)
+	to_decode <- gsub('[+]', ' ', to_decode)
+
+	# Copy image file from temp folder
+	file.copy(from=from, to=to_decode, overwrite=TRUE)
+
+	cat('from: ', from, '\n')
+	cat('to: ', to_decode, '\n')
+	
+	# Remove version in temp
+	if(file.exists(from)) file.remove(from)
+}
