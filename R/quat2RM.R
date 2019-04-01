9d2dfae6fac063faed2f7937c985bdf229ebc525
Aaron Olsen <aaronolsen@users.noreply.github.com>
2019-04-01 15:24:24 -0400

    Created function to convert quaternion to rotation matrix

---
+++ R/quat2RM.R
@@ -0,0 +1,13 @@
+quat2RM <- function(q){
+
+	# http://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToMatrix/index.htm
+	# assumed input order is qx, qy, qz, qw
+	
+	RM <- cbind(
+		c(1 - 2*q[2]^2 - 2*q[3]^2, 2*q[1]*q[2] - 2*q[3]*q[4], 2*q[1]*q[3] + 2*q[2]*q[4]),
+		c(2*q[1]*q[2] + 2*q[3]*q[4], 1 - 2*q[1]^2 - 2*q[3]^2, 2*q[2]*q[3] - 2*q[1]*q[4]),
+		c(2*q[1]*q[3] - 2*q[2]*q[4], 2*q[2]*q[3] + 2*q[1]*q[4], 1 - 2*q[1]^2 - 2*q[2]^2)
+	)
+	
+	RM
+}
