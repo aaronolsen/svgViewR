f2489ed848d25d08ae09a17bdfb563655f28cdfb
Aaron Olsen <aaronolsen@users.noreply.github.com>
2019-04-01 15:24:11 -0400

    Created function to apply quaternion

---
+++ R/applyQuat.R
@@ -0,0 +1,10 @@
+applyQuat <- function(p, q){
+	## Copied from https://gamedev.stackexchange.com/questions/28395/rotating-vector3-by-a-quaternion
+
+	if(is.null(dim(p))){
+		return(2 * sum(q[1:3] * p) * q[1:3] + (q[4]^2 - sum(q[1:3]*q[1:3])) * p + 2 * q[4] * cprod(q[1:3], p))
+	}else if(length(dim(p) == 2)){
+		for(i in 1:nrow(p)) p[i,] <- applyQuat(p[i,], q)
+		return(p)
+	}
+}
