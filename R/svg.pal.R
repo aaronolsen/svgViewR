fb8f2845912e5a982e0860d92dbbe0ba511a636a
Aaron Olsen <aaronolsen@users.noreply.github.com>
2019-11-20 15:32:10 -0500

    Update svg.pal.R

--- R/svg.pal.R
+++ R/svg.pal.R
@@ -2,7 +2,8 @@ svg.pal <- function(n, yellow = TRUE, alpha = 1){
 	
 	# Set colors
 	cols <- c("#E41A1C","#377EB8","#4DAF4A","#984EA3","#FF7F00","#FFFF33","#A65628","#F781BF",
-		"#999999","#66C2A5","#FC8D62","#8DA0CB","#E78AC3","#A6D854","#FFD92F","#E5C494","#B3B3B3")
+		"#999999","#66C2A5","#FC8D62","#8DA0CB","#E78AC3","#A6D854","#FFD92F","#E5C494","#B3B3B3",
+		"#941751", "#011993")
 
 	if(alpha < 1){
 		for(i in 1:length(cols)){
