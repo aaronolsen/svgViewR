ba0a71d2539b351cbf64f3fc49bcbb328c126e57
Aaron Olsen <aaronolsen@users.noreply.github.com>
2020-03-10 17:31:32 -0400

    Create addMeshes.R

---
+++ R/addMeshes.R
@@ -0,0 +1,24 @@
+addMeshes <- function(mesh1, mesh2 = NULL, mesh3 = NULL, mesh4 = NULL, mesh5 = NULL, mesh6 = NULL){
+
+	if(is.null(mesh2)) return(mesh1)
+
+	if(!is.null(mesh6)) return(addMeshes(addMeshes(addMeshes(addMeshes(addMeshes(mesh1, mesh2), mesh3), mesh4), mesh5), mesh6))
+	if(!is.null(mesh5)) return(addMeshes(addMeshes(addMeshes(addMeshes(mesh1, mesh2), mesh3), mesh4), mesh5))
+	if(!is.null(mesh4)) return(addMeshes(addMeshes(addMeshes(mesh1, mesh2), mesh3), mesh4))
+	if(!is.null(mesh3)) return(addMeshes(addMeshes(mesh1, mesh2), mesh3))
+
+	# Combine vertices
+	vertices <- rbind(mesh1$vertices, mesh2$vertices)
+
+	# Advance indices for faces in mesh2 by number of vertices in mesh1
+	mesh2$faces <- mesh2$faces + nrow(mesh1$vertices)
+
+	# Combine faces
+	faces <- rbind(mesh1$faces, mesh2$faces)
+
+	# Combine normals
+	normals <- rbind(mesh1$normals, mesh2$normals)
+	
+	# Create new mesh
+	new_mesh <- list('vertices'=vertices, 'faces'=faces, 'normals'=normals)
+}
