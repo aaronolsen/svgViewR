108c54a3d6624f56122c6f0e4f7f5bf66be4766c
Aaron Olsen <aaronolsen@users.noreply.github.com>
2020-10-02 08:24:23 -0400

    Update svg.sphere.R

--- R/svg.sphere.R
+++ R/svg.sphere.R
@@ -45,6 +45,7 @@ svg.sphere <- function(center = NULL, ends = NULL, radius = NULL, width = NULL, 
 
 		# Add vertices
 		svgviewr_env$svg$mesh[[add_at]] <- list()
+		svgviewr_env$svg$mesh[[add_at]]$name <- name
 		svgviewr_env$svg$mesh[[add_at]]$vertices <- t(vertices)
 		svgviewr_env$svg$mesh[[add_at]]$faces <- t(faces)
 		svgviewr_env$svg$mesh[[add_at]]$col <- setNames(webColor(col), NULL)
